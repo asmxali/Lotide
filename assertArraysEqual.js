d62b240cacae0215c39598459f75242c8ec86c53
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-02-04 22:00:56 +0000

    using eqArrays functions inside new function

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+
+const assertArraysEqual = function(actual, expected) {
+
+  let new1 = eqArrays(actual,expected);
+
+  if (new1 === true) {
+    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(` 🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (firstArray, secondArray) {
+  let count =0;
+  for (let i = 0; i <=firstArray.length-1; i ++) {
+    if (firstArray[i] === secondArray[i]) {
+      count +=1;
+    }
+  }
+  if (count === firstArray.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
