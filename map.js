6e03c1380480beea3c5201a6f016cfc698bf2aee
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-02-06 18:17:24 +0000

    using the map higher order function

---
+++ map.js
@@ -0,0 +1,47 @@
+// Function Definition
+// Our map function will take in two arguments:
+
+// An array to map
+// A callback function
+// The map function will return a new array based on the results of the callback function.
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+
+const eqArrays = function (firstArray, secondArray) {
+  let count =0;
+  for (let i = 0; i <=firstArray.length-1; i ++) {
+    if (firstArray[i] === secondArray[i]) {
+      count +=1;
+    }
+  }
+  if (count === firstArray.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const assertArraysEqual = function(actual, expected) {
+
+  let new1 = eqArrays(actual,expected);
+
+  if (new1 === true) {
+    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(` 🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertArraysEqual(results1,['g','c','t','m','t']);
+assertArraysEqual(results1,['g','c','t','m']);
+assertArraysEqual(results1,[]);
